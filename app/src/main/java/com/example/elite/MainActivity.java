e83556e0b2ad5118518902ae930400166c8f9c15
Fruzi1k <artem.grun2004pl@gmail.com>
2025-08-20 17:58:26 +0200

    Initial commit

---
+++ app/src/main/java/com/example/elite/MainActivity.java
@@ -0,0 +1,119 @@
+package com.example.elite;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.progressindicator.LinearProgressIndicator;
+import com.google.android.material.transition.MaterialArcMotion;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.util.UUID;
+
+public class MainActivity extends AppCompatActivity {
+    StorageReference storageReference;
+    LinearProgressIndicator progress;
+    Uri image;
+    MaterialButton selectImage, uploadImage;
+    ImageView imageView;
+
+    private  final ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+        @Override
+        public void onActivityResult(ActivityResult result) {
+            if(result.getResultCode() == RESULT_OK){
+
+                if (result.getData() != null) {
+                    image = result.getData().getData();
+                    uploadImage.setEnabled(true);
+                    Glide.with(getApplicationContext()).load(image).into(imageView);
+                }
+            }else{
+                Toast.makeText(MainActivity.this,"Please select an image", Toast.LENGTH_SHORT).show();
+            }
+        }
+    });
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
+
+        FirebaseApp.initializeApp( MainActivity.this);
+        storageReference = FirebaseStorage.getInstance().getReference();
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        progress = findViewById(R.id.progress);
+        imageView = findViewById(R.id.imageView);
+        selectImage = findViewById(R.id.button_selectImage);
+        uploadImage = findViewById(R.id.button_uploadImage);
+
+        selectImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                activityResultLauncher.launch(intent);
+            }
+        });
+        
+        uploadImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                uploadImage(image);
+            }
+        });
+
+    }
+
+    private void uploadImage(Uri image) {
+        StorageReference reference = storageReference.child("images/" + UUID.randomUUID().toString());
+        reference.putFile(image).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                Toast.makeText(MainActivity.this, "Image uploaded successfully", Toast.LENGTH_SHORT).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(MainActivity.this, "There was an errorwhile uploading", Toast.LENGTH_SHORT).show();
+            }
+        }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onProgress(@NonNull UploadTask.TaskSnapshot snapshot) {
+                progress.setMax(Math.toIntExact(snapshot.getTotalByteCount()));
+                progress.setProgress(Math.toIntExact(snapshot.getTotalByteCount()));
+
+
+            }
+        });
+    }
+}
